b67109f78d06eaec5c7e397fd97329679e5f2334
Zulfa Aliyah <zulfaliyah22@gmail.com>
2020-10-15 12:46:44 +0700

    Initial Commit

---
+++ MindfulMinutes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MindfulMinutes
+//
+//  Created by Zulfa Aliyah on 15/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
